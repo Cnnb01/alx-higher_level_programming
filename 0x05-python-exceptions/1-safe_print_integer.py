dcd9818a201ed16befe8e64816484a61d9308f02
Cnnb01 <nyamambanyaguch@gmail.com>
2023-12-18 21:52:48 +0300

    exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(int(value)))
-    except TypeError as e:
-        print(e)
+        print("{:d}".format(int(value)))
+        return True
+    except (ValueError, TypeError):
+        return False
