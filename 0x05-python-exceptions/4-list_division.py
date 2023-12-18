39ddbd5dc94dd27f3a2edc6a129ee45997644d93
Cnnb01 <nyamambanyaguch@gmail.com>
2023-12-18 21:02:33 +0300

    exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3    
+def list_division(my_list_1, my_list_2, list_length):
+    quot = []
+    try:
+        if len(my_list_1) < list_length or len(my_list_2) < list_length:
+            print("out of range")
+            return quot
+        for i, j in zip(my_list_1[:list_length], my_list_2[:list_length]):
+            try:
+                result = i/j
+                quot.append(result)
+            except ZeroDivisionError:
+                print("division by 0")
+                quot.append(0)
+    except TypeError:
+        print("wrong type")
+        quot.append(0)
+    finally:
+        return quot
