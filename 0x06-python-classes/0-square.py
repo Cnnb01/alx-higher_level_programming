dbb8bcbcb302b24db973c7ebb40f20025e2eb9e4
Cnnb01 <nyamambanyaguch@gmail.com>
2023-12-20 10:14:58 +0300

    a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """This class represents a square with height and width attributes."""
+    def __init__(self, height=0, width=0):
+        """
+    Initialize the square with the given height and width.
+    Args:
+    height (int): The height of the square.
+    width (int): The width of the square.
+        """
+        self.height = height
+        self.width = width
