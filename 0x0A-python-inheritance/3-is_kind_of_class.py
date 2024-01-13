201fdb1faf38eb7780820ee0186e2b5b00135f92
Cnnb01 <nyamambanyaguch@gmail.com>
2024-01-13 21:58:37 +0300

    inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of, or an instance
+    of a subclass of a specific class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if the object is an instance of the specified
+        class or its subclass; otherwise, False.
+    """
+    if isinstance(obj, a_class):
+        return True
+
+    return False
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """
+    Check if an object inherits off a specific class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to check inheritance against.
+
+    Returns:
+        bool: True if the object inherits off the
+        specified class; otherwise, False.
+    """
+    actual_class = type(obj)
+
+    if issubclass(actual_class, a_class):
+        return True
+
+    return False
