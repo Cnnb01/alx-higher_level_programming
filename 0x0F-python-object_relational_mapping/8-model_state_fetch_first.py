12aa4a853739c43e4873fa5b4842a99f1f796bb0
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-14 02:58:11 +0300

    orm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,12 +3,12 @@
 """
 a script that lists all State objects from the database hbtn_0e_6_usa
 """
-if __name__ == "__main__":
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from sys import argv
+if __name__ == "__main__":
 
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
