4812760d91f46689be4e7b855832d5e9f0d675cf
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-14 03:00:05 +0300

    orm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         .format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True)
     my_session = sessionmaker(bind=engine)
-    state = my_session.query(State).order_by(State.id).first
+    state = my_session.query(State).order_by(State.id).first()
     if state is None:
         print("Nothing")
     else:
