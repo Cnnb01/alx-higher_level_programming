a05e42899d20c92a9053919058eb36a7e06c9bb8
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-24 19:04:56 +0300

    JS warmup

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const firstArg = process.argv;
+const secArg = process.argv;
+console.log(firstArg[2] + " is " + secArg[3])
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argus = process.argv;
+const val = parseInt(argus[2])
+if (!isNaN(val)){
+    console.log("My nummber: " + val)
+}else{
+    console.log ("Not a number")
+}
