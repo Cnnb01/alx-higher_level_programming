307d99ecd65e7f4dbb22a7049bdef93500101092
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-24 19:07:33 +0300

    JS warmup

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+// A script that prints a message depending of the number of arguments passed.
 
-const { argv } = require('node:process');
-if (argv.length <= 2) {
-  console.log("No argument");
-} else if (argv.length === 3) {
-  console.log("Argument found");
+const argsCount = process.argv.length - 2;
+
+if (argsCount <= 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
