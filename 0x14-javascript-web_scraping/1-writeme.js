70f6f7cdd8e7d18232003e4c84931078276b233b
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-22 13:19:10 +0300

    web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const data = process.argv[3];
+fs.writeFile(process.argv[2], data, 'utf-8', (error) => {
+  if (error) {
+    console.error(err);
+    return;
+  }
+});
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request (url, function (error, response, body) {
+ if (error) {
+  console.log(error);
+ } else {
+    const results = JSON.parse(body).results;
+    const moviesWithWedge = results.reduce((count, movie) => {
+        return movie.characters.find((character) => character.endsWith('/18/'))
+          ? count + 1 // if found
+          : count; // if not found
+      }, 0);
+      console.log(moviesWithWedge);
+   }
+});
